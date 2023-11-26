5a927ce4f1b7e908c6177af309e74453a37c64d5
muthuriKE <muthurikennedy082@gmail.com>
2023-11-26 13:07:18 +0300

    starter code

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h3 className="text-3xl font-bold"> State Management</h3>
+    </main>
+  )
+}
